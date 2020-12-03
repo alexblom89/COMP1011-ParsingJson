7b35e2e7cb08c6a70b2a6083d1923923a84745ae
alexblom89 <55286258+alexblom89@users.noreply.github.com>
2020-12-02 20:51:20 -0500

    Add JavaFX, Create DictionaryAPIUtility.

---
+++ src/Utilities/DictionaryAPIUtility.java
@@ -0,0 +1,24 @@
+package Utilities;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class DictionaryAPIUtility {
+
+    public static void getWordFromSearch(String searchText) throws IOException, InterruptedException {
+        String searchURL = "https://wordsapiv1.p.rapidapi.com/words/*" + searchText + "*";
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(searchURL))
+                .header("x-rapidapi-key", "3fe8312e84mshbade5e19de86673p182f9cjsn4659958f908e")
+                .header("x-rapidapi-host", "wordsapiv1.p.rapidapi.com")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        HttpResponse<Path> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofFile(Paths.get("./src/JSONFiles/wordFiles.json")));
+    }
+}
